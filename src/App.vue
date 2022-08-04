f268cf50946bc8b104a8c06bd5d7a216d460c4dd
Marko <repository@idweb.space>
2022-08-04 15:42:55 +0300

    Installation bootsatrap

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+  <HeaderPage/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script> 
+import HeaderPage from './components/HeaderPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderPage
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
