fc50744d472a848369ced04f5655ef9656993bdd
Marko <repository@idweb.space>
2022-08-05 11:45:29 +0300

    Create component footer and header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
-<template> 
-  <HeaderPage/>
+<template>
+  <HeaderPage />
+  <FooterPage />
 </template>
 
-<script> 
-import HeaderPage from './components/HeaderPage.vue'
+<script>
+import HeaderPage from "./components/HeaderPage.vue";
+import FooterPage from "./components/FooterPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HeaderPage
-  }
-}
+    HeaderPage,
+    FooterPage,
+  },
+};
 </script>
 
-<style>
-#app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; */
-}
-</style>
+<style></style>
--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,40 +1,109 @@
 <template>
-<div>
-  <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
-    <b-dropdown-item>First Action</b-dropdown-item>
-    <b-dropdown-item>Second Action</b-dropdown-item>
-    <b-dropdown-item>Third Action</b-dropdown-item>
-    <b-dropdown-divider></b-dropdown-divider>
-    <b-dropdown-item active>Active action</b-dropdown-item>
-    <b-dropdown-item disabled>Disabled action</b-dropdown-item>
-  </b-dropdown>
-      <b-button variant="outline-secondary">Button</b-button>
-</div> 
+  <div class="background">
+    <div class="header">
+      <b-dropdown
+        id="dropdown-1"
+        variant="outline-light"
+        text="FoodZero"
+        no-caret
+        class="logo"
+      >
+        <b-dropdown-item>First Action</b-dropdown-item>
+        <b-dropdown-item>Second Action</b-dropdown-item>
+        <b-dropdown-item>Third Action</b-dropdown-item>
+      </b-dropdown>
+      <div class="header-right">
+        <a href="#contact">+86 852 346 000</a>
+        <b-button variant="outline-light">Button</b-button>
+      </div>
+    </div>
+
+    <div class="title">
+      <p>Deep Sea Snow white</p>
+      <p>Cod Fillet</p>
+    </div>
+      <router-view></router-view>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HeaderPage',
+  name: "HeaderPage",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+* {
+  box-sizing: border-box;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.title {
+  font-size: 45px;
+  text-align: center;
+  color: white;
+  line-height: 34px;
+  margin-top: 90px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.background {
+  background: transparent url(../assets/images/fruit.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 40vh;
 }
-a {
-  color: #42b983;
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.header {
+  padding: 20px 50px;
+}
+
+.header a {
+  float: left;
+  color: white;
+  text-align: center;
+  padding: 7px;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 25px;
+  border-radius: 4px;
+  font-weight: bolder;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.header-right {
+  float: right;
+}
+
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+
+  .header-right {
+    float: none;
+  }
 }
 </style>
